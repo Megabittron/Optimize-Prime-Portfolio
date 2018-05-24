85854ce614facdb6c0d2b814c678e58d1a8dbb5b
ahnafy <yasin011@morris.umn.edu>
2018-05-24 17:52:10 -0500

    add angular 6 webapp template

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,42 @@
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {throwIfAlreadyLoaded} from './module-import-guard';
+import {LoggerService} from './shared/logger.service';
+import {NavComponent} from './nav/nav.component';
+import {FooterComponent} from './footer/footer.component';
+import {SharedModule} from '../shared/modules/shared.module';
+import {RouterModule} from '@angular/router';
+import {SearchBarComponent} from './search-bar/search-bar.component';
+import {Error404Component} from './error404/error-404.component';
+import {ProgressBarService} from './shared/progress-bar.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    SharedModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    NavComponent,
+    FooterComponent
+  ],
+  declarations: [
+    NavComponent,
+    FooterComponent,
+    SearchBarComponent,
+    Error404Component
+  ],
+  providers: [
+    LoggerService,
+    ProgressBarService
+  ]
+})
+
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}
